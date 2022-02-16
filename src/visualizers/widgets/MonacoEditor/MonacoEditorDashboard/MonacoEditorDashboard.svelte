a602bf742f73672fb394244e501d893164225f59
Umesh Timalsina <umesh.timalsina@vanderbilt.edu>
2022-02-15 20:20:46 -0600

    WIP- Create Editor

---
+++ src/visualizers/widgets/MonacoEditor/MonacoEditorDashboard/MonacoEditorDashboard.svelte
@@ -0,0 +1,43 @@
+<script>
+
+    import {onMount} from 'svelte';
+    let eventElement, editor;
+    export let monaco;
+
+    onMount(() => {
+        setTimeout(() => {
+            editor = monaco.editor.create(
+            document.getElementById('monaco-editor'),
+            {
+                model: monaco.editor.createModel('console.log("hi");', 'javascript'),
+                minimap: {
+                    enabled: true
+                },
+                dimension: {
+                    width: 800,
+                    height: 800
+                },
+                autosize: true
+            }
+        );
+        }, 0);
+
+    });
+
+    export function resize(width, height) {
+        if (monaco && editor) {
+            console.log('here');
+            editor.updateOptions({
+                dimension: {
+                    width: width,
+                    height: height
+                }
+            });
+        }
+    }
+
+</script>
+<main bind:this={eventElement}>
+    <div id="monaco-editor">
+    </div>
+</main>
